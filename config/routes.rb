0d96b50a8c63d397ea13dc8b96ff9a00375a5103
Ben Treglown <treggy89@gmail.com>
2022-11-12 16:24:35 +1100

    Restaraunt sectino complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurant#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'restaurants/new', to: 'restaurant#new'
+  post 'restaurants', to: 'restaurant#create'
+
+  get 'restaurants/:id', to: 'restaurant#show', as: :restaurant
 end
